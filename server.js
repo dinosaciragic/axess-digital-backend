078f1acc27d4119ed56c1b585ad426df279908a0
Dino Saciragic <dino@appit.ba>
2021-10-02 18:55:05 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const config = require('config');
+
+const app = express();
+
+// Init middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.json({ msg: 'Welcome to the Axess Digital API' });
+});
+
+const PORT = config.get('PORT');
+
+app.listen(PORT, () => console.log(`Server on ${PORT}`));
