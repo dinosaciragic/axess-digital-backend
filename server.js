6afe09488e1da9541a0ce15df99b533a73721dfa
Dino Saciragic <dino@appit.ba>
2021-10-03 14:32:05 +0200

    all routes and API done

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.get('/', (req, res) => {
 app.use('/api/orders', require('./routes/orders'));
 app.use('/api/products', require('./routes/products'));
 app.use('/api/suppliers', require('./routes/suppliers'));
+app.use('/api/categories', require('./routes/categories'));
 
 const PORT = config.get('PORT');
 
